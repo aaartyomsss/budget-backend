3ab38cef7b392c8039b75143f055affef3a87622
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2021-06-14 08:56:19 +0300

    Bug fix

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,37 +1,48 @@
-const googleRouter = require('express').Router()
-const UserGoogle = require('../models/UserGoogle')
-const jwt = require('jsonwebtoken')
-const config = require('../utils/config')
+const googleRouter = require("express").Router();
+const UserGoogle = require("../models/UserGoogle");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 
+googleRouter.post("/", async (req, res) => {
+  const body = req.body;
 
+  let user = await UserGoogle.findOne({ googleId: body.googleId }).populate(
+    "personalPlan"
+  );
 
-googleRouter.post('/', async (req, res) => {
-    console.log(req.body)
-    const body = req.body 
+  if (user) {
+    const token = jwt.sign(user.toJSON(), config.SECRET);
+    res.send({
+      token,
+      googleId: user.googleId,
+      email: user.email,
+      name: user.name,
+      id: user._id,
+      familyPlans: user.familyPlans,
+    });
+  } else {
+    user = new UserGoogle({
+      googleId: body.googleId,
+      email: body.email,
+      name: body.name,
+      image: body.imageUrl,
+    });
 
-    let user = await UserGoogle.findOne({ googleId: body.googleId}).populate('personalPlan')
+    const token = jwt.sign(user.toJSON(), config.SECRET);
 
-    if(user) {
-        const token = jwt.sign(user.toJSON(), config.SECRET)
-        res.send({ token, googleId: user.googleId, email: user.email, name: user.name, id: user._id })
-    } else {
-        user = new UserGoogle({
-            googleId: body.googleId,
-            email: body.email,
-            name: body.name,
-            image: body.imageUrl
-        })
-
-        const token = jwt.sign(user.toJSON(), config.SECRET)
-
-        try {
-            await user.save()
-            res.send({ token, googleId: user.googleId, email: user.email, name: user.name, id: user._id })
-        } catch (e) {
-            console.error(e.message)
-        }
+    try {
+      await user.save();
+      res.send({
+        token,
+        googleId: user.googleId,
+        email: user.email,
+        name: user.name,
+        id: user._id,
+      });
+    } catch (e) {
+      console.error(e.message);
     }
+  }
+});
 
-})
-
-module.exports = googleRouter
+module.exports = googleRouter;
--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,47 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/User')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/User");
 
-loginRouter.post('/', async (req, res) => {
-    const body = req.body
+loginRouter.post("/", async (req, res) => {
+  const body = req.body;
 
-    const user = await User.findOne({ username: body.username }).populate('personalPlan')
-    const passwordValidation = user === null
-        ? false
-        : await bcrypt.compare(body.password, user.passwordHash)
+  const user = await User.findOne({ username: body.username }).populate(
+    "personalPlan"
+  );
+  const passwordValidation =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
 
-    if(!(user && passwordValidation)){
-        return res.status(401).json({
-            error: 'Invalid password or username'
-        })
-    }
+  if (!(user && passwordValidation)) {
+    return res.status(401).json({
+      error: "Invalid password or username",
+    });
+  }
 
-    if(!user.confirmed){
-        return res.status(403).json({ error: 'Your account is not confirmed'})
-    }
+  if (!user.confirmed) {
+    return res.status(403).json({ error: "Your account is not confirmed" });
+  }
 
-    const userForToken = {
-        username: user.username,
-        name: user.name,
-        id: user._id
-    }
+  const userForToken = {
+    username: user.username,
+    name: user.name,
+    id: user._id,
+  };
 
-    const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET);
 
-    res.status(200).send({ token, username: user.username, name: user.name, personalPlan: user.personalPlan, id: user._id })
-})
+  res
+    .status(200)
+    .send({
+      token,
+      username: user.username,
+      name: user.name,
+      personalPlan: user.personalPlan,
+      id: user._id,
+      familyPlans: user.familyPlans,
+    });
+});
 
-module.exports = loginRouter
+module.exports = loginRouter;
